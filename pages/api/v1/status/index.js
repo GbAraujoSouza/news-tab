dff46b0d99f960c7e37f7517543cac198f71d130
Gabriel de Araujo de Souza <gabriel.souzaaraujo109@gmail.com>
2024-11-05 17:09:09 +0000

    feat: finish `status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 import database from "@/infra/database.js";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows[0]);
-  response.status(200).json({ chave: "Você contactou o endpoint status" });
+  const updatedAt = new Date().toISOString();
+
+  const versionQueryResult = await database.query("SHOW server_version;");
+  const version = versionQueryResult.rows[0].server_version;
+
+  const maxConnectionsQueryResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const maxConnections = maxConnectionsQueryResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const usedConnectionsQueryResult = await database.query({
+    text: `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+  const openedConnections = usedConnectionsQueryResult.rows[0].count;
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version,
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnections,
+      },
+    },
+  });
 }
 
 export default status;
